d4dd96351f03bfec91049e637cf61677245bb6e2
dwpeterson3 <pet08043@byui.edu>
2024-04-27 14:54:38 -0400

    Learning Activity C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+
+        int x = int.Parse(grade);
+        
+        string letter = "";
+
+        if (x >= 90)
+        {
+           letter = "A";
+        }
+        else if (x < 90 && x >= 80)
+        {
+            letter = "B";
+            
+        }        
+        else if (x < 80 && x >= 70)
+        {
+            letter = "C";
+            
+        }
+        else if (x < 70 && x >= 60)
+        {
+            letter = "D";
+            
+        }
+        else
+        {
+            letter = "F";
+        
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (x >= 70)
+        {
+            Console.WriteLine("Congratulations!! You Passed! ");
+
+        } 
+        else 
+        {
+            Console.WriteLine("Yikes! Study harder next time! ");
+        }
+
+
     }
 }
