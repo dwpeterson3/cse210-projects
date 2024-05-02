ad6313ba38ebf55553cd946b4384ac33b5df53c9
dwpeterson3 <pet08043@byui.edu>
2024-05-01 20:34:02 -0400

    Prep 3 addition

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+
+        int magicNumber = random.Next(1,20);
+
+        int guess = 0;
+
+        int tracker = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            tracker++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("Congratualations! You guessed the magic number! ");
+
+                Console.WriteLine($"It took you {tracker} tries to guess the magic number! ");            
+            }            
+
+        }
+
     }
 }
